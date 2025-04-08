d486ee47ba2b9201ad0018dd8c645fbc96e0af36
Muaawiya <snippetcms@gmail.com>
2025-04-08 21:21:00 +0500

    route updated

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use PakPromo\FileManager\Http\Controllers\FileLibraryController;
+use PakPromo\FileManager\Http\Controllers\FileManagerController;
 
-Route::get('/filemanager/uploader.min.js', [FileLibraryController::class, 'uploader'])->name('filemanager.uploader');
+Route::get('/filemanager/uploader.min.js', [FileManagerController::class, 'uploader'])->name('filemanager.uploader');
 
-Route::post('/dropzone/upload', [FileLibraryController::class, 'upload'])->name('filemanager.dropzone.upload');
-Route::post('/dropzone/delete', [FileLibraryController::class, 'delete'])->name('filemanager.dropzone.delete');
+Route::post('/dropzone/upload', [FileManagerController::class, 'upload'])->name('filemanager.dropzone.upload');
+Route::post('/dropzone/delete', [FileManagerController::class, 'delete'])->name('filemanager.dropzone.delete');
